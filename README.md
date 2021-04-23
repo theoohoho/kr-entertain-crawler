ce11eb11425b91399240ac72f9de03f305459249
theoohoho <topslimer@gmail.com>
2021-04-23 14:23:09 +0800

    feat: add store_in_db in crawler.py

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 Web Crawler 爬取 PTT KR_ENTERTAIN board，收集所有韓綜訊息內容
 
 ## TODO
-- [ ] Should store data into database
+- [x] Should store data into database
 - [x] Should crawling multi page
--- crawler.py
+++ crawler.py
@@ -3,6 +3,8 @@ import asyncio
 import re
 from bs4 import BeautifulSoup
 from enum import Enum
+from model import TV_EPISODE, TV_SHOW, TV_CHANNEL
+from database import get_db_session
 
 
 class SourcePlatform(Enum):
@@ -11,6 +13,14 @@ class SourcePlatform(Enum):
     IQIYI = 'iqiyi'
     KKTV = 'kktv'
     YOUTUBE = 'youtu'
+
+
+# The element index in TV SHOW Tittle
+class TitleInfoIndex:
+    publish = 1
+    channel = 2
+    title = 3
+    episode = 4
 
 
 class Crawler:
@@ -27,7 +37,7 @@ class Crawler:
         return cls._instance
 
     def __init__(self):
-        pass
+        self.crawler_result = []
 
     async def fetch_source(self):
         """Fetch target data source
@@ -104,16 +114,14 @@ class Crawler:
 
     async def crawl(self, page_range: int = 0):
         self._session = aiohttp.ClientSession()
-        article_content = []
         # select crawl range that start from last page
         last_page = await asyncio.create_task(self.get_last_page())
-        target_pages = [page for page in range(
-            last_page, last_page-page_range-1, -1)]
+        target_pages = [page for page in range(last_page, last_page-page_range-1, -1)]
         print(f'Last page is {str(last_page)}')
         print(f'Target page to crawl: {str(target_pages)}')
         for target_page in target_pages:
             title_links = await asyncio.create_task(self.parse(target_page))
-            article_content.extend(await self.parse_articles(title_links))
+            self.crawler_result.extend(await self.parse_articles(title_links))
         await self._session.close()
 
     def extract_data(self):
@@ -121,25 +129,97 @@ class Crawler:
         """
         pass
 
-    def clean_data(self):
+    def get_tv_metadata(self, db_session) -> tuple:
+        tv_channels = db_session.query(TV_CHANNEL).all()
+        tv_shows = db_session.query(TV_SHOW.id, TV_SHOW.title).select_from(TV_SHOW).all()
+        return (tv_channels, tv_shows)
+
+    def clean_data(self, db_session, tv_channels: list) -> list:
         """Expect to clean html data, and extract tv source link
         """
-        pass
+        print('To clean crawled data....')
+        channel_ht = {}
+        cleaned_result, channel_regex = [], []
 
-    def parse_to_schema(self):
+        for channel in tv_channels:
+            channel_ht[channel.name] = channel.id
+            channel_regex.append(channel.name)
+
+        channel_regex = f'({"|".join(channel_regex)})'
+
+        for result in self.crawler_result:
+            title_info = result.get('article_title')
+            tv_channel = re.search(channel_regex, title_info)
+            if not tv_channel:
+                print(f"Data can\'t parsed, the title is: {title_info}")
+                continue
+
+            # replace title info that match with tv channel name to blank
+            title_info = re.sub(channel_regex, '', title_info.upper())
+            tv_channel = tv_channel.group()
+            # select specific index
+            title_info = title_info.split(' ')
+            video_source_link = ','.join(result.get('video_urls'))
+
+            cleaned_result.append(dict(
+                tv_show_title=title_info[TitleInfoIndex.title],
+                episode_name=title_info[TitleInfoIndex.episode],
+                source_link=video_source_link,
+                publish_date=title_info[TitleInfoIndex.publish],
+                description='',
+                tv_channel=tv_channel,
+                tv_channel_id=channel_ht.get(tv_channel)
+            ))
+
+        return cleaned_result
+
+    def parse_to_schema(self, db_session, shows, complete_result: list) -> None:
         """Parse data to table schema
         """
-        pass
+        print('To parsed cleaned data into table model....')
+        existed_tvshow_ht = {}
 
-    def store_in_db(self):
+        for show in shows:
+            existed_tvshow_ht[show.title] = show.id
+
+        for tvshow_info in complete_result:
+            tvshow_title = tvshow_info.get('tv_show_title')
+            tv_show_id = existed_tvshow_ht.get(tvshow_title)
+
+            if tv_show_id is None:
+                created_tvshow = TV_SHOW(
+                    title=tvshow_title,
+                    description=tvshow_info.get('description'),
+                    tv_channel_id=tvshow_info.get('tv_channel_id')
+                )
+                db_session.add(created_tvshow)
+                db_session.flush()
+                db_session.refresh(created_tvshow)
+                tv_show_id = created_tvshow.id
+
+            created_tv_episode = TV_EPISODE(
+                name=tvshow_info.get('episode_name'),
+                publish_date=tvshow_info.get('publish_date'),
+                tv_show_id=tv_show_id,
+                source_link=tvshow_info.get('video_source_link')
+            )
+            db_session.add(created_tv_episode)
+
+    def store_in_db(self) -> None:
         """Store data into database
         """
-        pass
+        with get_db_session() as db_session:
+            channels, shows = self.get_tv_metadata(db_session)
+            complete_result = self.clean_data(db_session, channels)
+            self.parse_to_schema(db_session, shows, complete_result)
+            db_session.commit()
+            print('Successfully store data into database....')
 
     def run(self):
         """Run crawler to do right thing
         """
         asyncio.run(self.crawl())
+        self.store_in_db()
 
 
 if __name__ == "__main__":
